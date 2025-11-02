f5ec401f9336f9217a664a6bdf997ec2906eec24
ivanpoveda <ivanpoveda0206@gmail.com>
2025-11-02 09:09:00 -0600

    Creacion de modelo Vecino

---
+++ RegistroComunitario/Models/Vecino.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroComunitario.Models
+{
+    public class Vecino
+    {
+        [Required]
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "El nombre es obligatorio")]
+        [MinLength(3, ErrorMessage = "El nombre debe tener al menos 3 caracteres")]
+        public string Nombre { get; set; }
+
+        [Required]
+        public string Apellidos { get; set; }
+
+        [Phone]
+        public string Telefono { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "El correo electrónico no es válido")]
+        public string Correo { get; set; }
+
+        [Required]
+        public string Genero { get; set; }
+
+        [Range(1, 120, ErrorMessage = "La edad debe estar entre 1 y 120")]
+        public int Edad { get; set; }
+    }
+}
