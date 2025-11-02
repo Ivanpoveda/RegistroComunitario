907229969c62373bb1107f85702a66e7efa4058c
ivanpoveda <ivanpoveda0206@gmail.com>
2025-11-02 09:10:29 -0600

    Creacion de la carpeta Data y se agrega RepositorioVecinos

---
+++ RegistroComunitario/Data/RepositorioVecinos.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using RegistroComunitario.Models;
+
+namespace RegistroComunitario.Data
+{
+    public static class RepositorioVecinos
+    {
+        private static List<Vecino> listaVecinos = new List<Vecino>();
+
+        public static void Agregar(Vecino v)
+        {
+            listaVecinos.Add(v);
+        }
+
+        public static List<Vecino> ObtenerTodos()
+        {
+            return listaVecinos;
+        }
+    }
+}
