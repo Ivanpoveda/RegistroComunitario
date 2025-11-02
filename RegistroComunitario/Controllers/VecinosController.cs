514445136d5467ea6a8eaf0cbdb46d25be00124b
ivanpoveda <ivanpoveda0206@gmail.com>
2025-11-02 09:11:44 -0600

    Creacion de Controller

---
+++ RegistroComunitario/Controllers/VecinosController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RegistroComunitario.Models;
+using RegistroComunitario.Data;
+
+namespace RegistroComunitario.Controllers
+{
+    public class VecinosController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vecinos = RepositorioVecinos.ObtenerTodos();
+            return View(vecinos);
+        }
+
+        [HttpGet]
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Crear(Vecino v)
+        {
+            if (ModelState.IsValid)
+            {
+                RepositorioVecinos.Agregar(v);
+                return RedirectToAction("Index");
+            }
+            return View(v);
+        }
+    }
+}
