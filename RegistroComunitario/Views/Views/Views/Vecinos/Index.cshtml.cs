4b478ad0fd13f69c574e230a4afa45731e08babf
ivanpoveda <ivanpoveda0206@gmail.com>
2025-11-02 09:13:44 -0600

    Creacion de las vistas

---
+++ RegistroComunitario/Views/Views/Views/Vecinos/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RegistroComunitario.Views.Views.Views.Vecinos
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
